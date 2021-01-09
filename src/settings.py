a16c14b9ac4041858d5ab0e2e0b999d3011ec591
hughwin <hugh.winchester@gmail.com>
2021-01-09 11:08:41 +0000

    Added DAILY_DIR to settings.py

--- src/settings.py
+++ src/settings.py
@@ -5,4 +5,5 @@ USERNAME = "@nasa_bot"
 NASA_ADDRESS_IMAGES = "https://api.nasa.gov/planetary/apod?api_key=%s"
 ROOT = Path(__file__).parent.parent
 BASE_DIRECTORY = ROOT / "src"
+DAILY_DIR = BASE_DIRECTORY / "daily"
 DAILY_IMAGE = str(BASE_DIRECTORY / "daily" / "image.jpeg")
