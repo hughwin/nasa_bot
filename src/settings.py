50d6a237bcdf6bda868b82480da6fd0179d4f5ee
hughwin <hugh.winchester@gmail.com>
2021-01-09 11:05:37 +0000

    Fixed errors in settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
 BASE_ADDRESS = "https://botsin.space/"
 USERNAME = "@nasa_bot"
 NASA_ADDRESS_IMAGES = "https://api.nasa.gov/planetary/apod?api_key=%s"
+ROOT = Path(__file__).parent.parent
+BASE_DIRECTORY = ROOT / "src"
+DAILY_IMAGE = str(BASE_DIRECTORY / "daily" / "image.jpeg")
